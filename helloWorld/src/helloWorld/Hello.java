03b4a4f2712d243b416223310ba5ba1aadfe6529
Tomilla <Tomilla@users.noreply.github.com>
2014-09-05 21:08:19 +0800

    Add: a time-stamp
    - add formated date to system output stream.

--- helloWorld/src/helloWorld/Hello.java
+++ helloWorld/src/helloWorld/Hello.java
@@ -1,8 +1,15 @@
 package helloWorld;
 
+// add a *time-stamp*
+import java.text.SimpleDateFormat; 
+import java.util.Date;
+
 public class Hello {
 	public static void main(String args[]) {
-		System.out.println("Hello World!");
+		Date now = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd, yyyy HH:mm:ss");
+		String formattedDate = formatter.format(now);
+		System.out.println("Now time is " + formattedDate + "\nHello World!");
 	}
 
 }

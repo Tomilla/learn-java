d481a551bec8d5b9223c382167b12609634e8e59
Tomilla <Tomilla@users.noreply.github.com>
2014-09-05 21:16:54 +0800

    Add: allow pass argument to *main* method

--- helloWorld/src/helloWorld/Hello.java
+++ helloWorld/src/helloWorld/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		Date now = new Date();
 		SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd, yyyy HH:mm:ss");
 		String formattedDate = formatter.format(now);
-		System.out.println("Now time is " + formattedDate + "\nHello World!");
+		System.out.println("Now time is " + formattedDate + "\nHello " + args[0] +"!");
 	}
 
 }
